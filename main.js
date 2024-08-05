4179314c9a709d857db6bc5e153d42433a7b1d03
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-08-05 19:08:01 +0300

    Basic functionality established
    Need to add %,+/-,
    other than that everything works well.

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
 let firstNumber = null;
 let operator = null;
 let resetDisplay = false;
-
+let hasPressedEqual = false;
 
 function operate(operator,number1,number2){
     switch(operator){
         case '+':
             return number1 + number2;
-            break;
         case '-':
             return number1 - number2;
-            break;
         case '*':
             return number1 * number2;
-            break;
         case '/':
             return number1 / number2;
-            break;
-        default:
-            return 'Invalid operator';
     }
 }
 
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
+function shouldReset(resetDisplay){
+    if(resetDisplay){
+        display.textContent = '';
+        resetDisplay = false;
+    }
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-       
+        if (button.classList.contains('number')){
+            display.textContent += button.textContent;
+        } else if (button.classList.contains('operator')){
+            if (hasPressedEqual){
+                number2 = parseFloat(display.textContent);
+                operator = button.textContent;
+                resetDisplay = true;
+                shouldReset(resetDisplay);
+            }else {
+            number1 = parseFloat(display.textContent);
+            operator = button.textContent;
+            resetDisplay = true;
+            shouldReset(resetDisplay);
+            }
+        } else if (button.classList.contains('clear')){
+            number1 = null;
+            operator = null;
+            number2 = null;
+            resetDisplay = true;
+            shouldReset(resetDisplay);
+            hasPressedEqual = false;
+        } else if (button.classList.contains('equal')){
+            number2 = parseFloat(display.textContent);
+            display.textContent = operate(operator,number1,number2);
+            number1 = parseFloat(display.textContent);
+            resetDisplay = true
+            hasPressedEqual = true;
+        }
+        
+
+
     });
 });
 
